16ff62dc48857337ddb50e84eaed8277519d969e
ninja21 <ninja@ara.com>
2025-10-29 11:37:14 +0700

    up author name

--- additional_export_pdf_image/__manifest__.py
+++ additional_export_pdf_image/__manifest__.py
@@ -5,7 +5,7 @@
     "version": "17.0.0.0.0",
     "license": "OPL-1",
     "summary": "Additional Export PDF in All Module",
-    "author": "ARA Split Developers",
+    "author": "ARA SOFT Split Developers",
     "website": "",
     "support": "",
     "description": """Additional Export PDF in All Module""",
